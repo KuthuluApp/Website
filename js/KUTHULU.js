89fa9ff1650b6b21eb55003e040bfc8f0c47d7f9
KuthuluApp <141072562+KuthuluApp@users.noreply.github.com>
2023-08-06 16:53:14 -0400

    take 2

--- js/KUTHULU.js
+++ js/KUTHULU.js
@@ -3283,8 +3283,8 @@ async function makePost(p, skipRepost, isComment){
                 if (uriToTest.indexOf('?') > 0){
                     uriToTest = uriToTest.substring(0,uriToTest.indexOf('?'));
                 }
-                // Added Tenor.com per Mudbutt
-                if (uriToTest.split('.').pop().toLowerCase() === validImgExt[z] || uriToTest.indexOf('://tenor.com')){
+                // Added Tenor.com per Mudbutt (tk 2... 420 style lol)
+                if (uriToTest.split('.').pop().toLowerCase() === validImgExt[z] || uriToTest.indexOf('://tenor.com') > 0){
                     validImg = true;
                 }
             }
